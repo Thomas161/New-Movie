d2f7dc7cf9a530aba089629cf10c464ea25d3466
Tom Magin <magintc@mac.com>
2020-11-01 14:27:51 +1100

    #attributed artwork to unsplash artist

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import Search from "../src/components/Search";
 import Review from "../src/components/Review";
 import Navbar from "../src/components/Navbar";
+import Footer from "../src/components/Footer";
 import MovieCard from "../src/components/MovieCard";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Route, Switch, BrowserRouter as Router } from "react-router-dom";
@@ -18,6 +19,7 @@ class App extends Component {
               <Route exact path="/review" component={Review} />
               <Route exact path="/submitted" component={MovieCard} />
             </Switch>
+            <Footer />
           </div>
         </Router>
       </>
---
+++ src/components/Footer.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "../css/Footer.css";
+
+const Footer = () => {
+  return (
+    <>
+      <div id="footerContainer">
+        <p id="artwork">
+          artwork by:<a href="https://unsplash.com/@myke_simon"> Myke Simon</a>
+        </p>
+      </div>
+    </>
+  );
+};
+export default Footer;
