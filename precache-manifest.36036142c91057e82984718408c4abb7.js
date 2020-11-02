e6310bd40c7e3aec201c631e47d2568d6a48ad9d
Tom Magin <magintc@mac.com>
2020-11-02 20:27:10 +1100

    Updates

---
+++ precache-manifest.36036142c91057e82984718408c4abb7.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "f29334c5de1d7b22ffc112c4b4be80f2",
+    "url": "/New-Movie/static/media/cinema2.f29334c5.jpg"
+  },
+  {
+    "revision": "0906ba21568a0acf23380b8369453b1a",
+    "url": "/New-Movie/static/media/cinema.0906ba21.jpg"
+  },
+  {
+    "revision": "41655541bc46d3213501",
+    "url": "/New-Movie/static/js/runtime~main.752f14e7.js"
+  },
+  {
+    "revision": "5c693a37a0e3da2007bd",
+    "url": "/New-Movie/static/js/main.1258a5ae.chunk.js"
+  },
+  {
+    "revision": "e7c5de79516e54a0635d",
+    "url": "/New-Movie/static/js/2.26df3833.chunk.js"
+  },
+  {
+    "revision": "5c693a37a0e3da2007bd",
+    "url": "/New-Movie/static/css/main.d12b4790.chunk.css"
+  },
+  {
+    "revision": "e7c5de79516e54a0635d",
+    "url": "/New-Movie/static/css/2.890abb99.chunk.css"
+  },
+  {
+    "revision": "6cfda384af77810cb01af467c0854bc0",
+    "url": "/New-Movie/index.html"
+  }
+];
